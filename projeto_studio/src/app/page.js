1bb15042cfd74d490721f5a2daa8fdcf287202b1
Rafael Barros <rgracilianobarros@gmail.com>
2025-04-17 14:51:58 -0300

    Iniciando a estrutura -NextJS-

---
+++ projeto_studio/src/app/page.js
@@ -0,0 +1,10 @@
+import  Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold">Bem-vindo ao Studio RT</h1>
+      <Header />
+    </section>
+  )
+}
